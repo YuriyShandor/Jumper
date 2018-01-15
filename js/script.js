3039a963fb67f0ec3a2a7ac72574e5fd59f4b556
Yuriy Shandor <yuriyshandor@gmail.com>
2018-01-15 21:43:43 +0200

    Add Barrier. Mod_01

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var jumper = document.getElementById("jumper");
+var arena = document.getElementById("arena");
 
 function jumping(e) {
   var x = e.keyCode;
@@ -6,7 +7,14 @@ function jumping(e) {
     jumper.style.bottom = "170px";
   };
 
-  setTimeout("jumper.style.bottom = '100px'", 500);
+  setTimeout("jumper.style.bottom = '100px'", 300);
 }
 
 document.onkeydown = jumping;
+
+
+var div = document.createElement('div');
+div.className = "barrier-block";
+arena.appendChild(div);
+
+//etInterval("arena.appendChild(div)", 3000);
