010ce8b18f8f7c130ee0cd82798e748657f682f2
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-04 16:02:55 +0200

    Add Jumper. Mod_02

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
-var arena = document.getElementById('arena')
+var arena = document.getElementById('arena');
 var c = arena.getContext('2d');
 
 arena.width = 1024;
 arena.height = 600;
 
-c.fillRect(0, 500, 1024, 100);
+var canvasKeyCode = undefined;
 
+document.addEventListener('keydown', function(event) {
+  canvasKeyCode = event.keyCode;
+});
 
-// c.beginPath();
-// var jumper = c.arc(175, 475, 25, 0, Math.PI * 2, false);
-// c.fillStyle = "red";
-// c.strokeStyle = "#000";
-// c.fill();
-// c.stroke();
+document.body.addEventListener("keyup", function(e) {
+    canvasKeyCode = undefined;
+});
+
+var gravity = 0.1;
+var friction = 0.8;
 
 function Circle(x, y, radius) {
   this.x = x;
   this.y = y;
-  // this.dx = dx;
-  // this.dy = dy;
+  this.velX = 0;
+  this.velY = 0;
+  this.speed = 3;
   this.radius = radius;
+  this.jumping = false;
 
   this.draw = function() {
     c.beginPath();
@@ -27,12 +32,87 @@ function Circle(x, y, radius) {
     c.fillStyle = "red";
     c.stroke();
     c.fill();
-    c.closePath();
   };
 
   this.move = function() {
+
+    if (canvasKeyCode == 38 || canvasKeyCode == 32) {
+      // jump
+      if(!this.jumping){
+        this.jumping = true;
+        this.velY = -this.speed*2;
+      };
+    };
+    if (canvasKeyCode == 39) {
+      // move right
+      if (this.velX < this.speed) {
+        this.x++;
+      };
+    };
+    if (canvasKeyCode == 37) {
+      // move left
+      if (this.velX > -this.speed) {
+        this.velX--;
+      };
+    };
+
+    this.velX *= friction;
+
+    this.velY += gravity;
+
+    this.x += this.velX;
+    this.y += this.velY;
+
+    if (this.x >= arena.width) {
+      this.x = arena.width;
+    } else if (this.x <= 0) {
+      this.x = 0;
+    };
+
+    if(this.y > 475){
+      this.y = 475;
+      this.jumping = false;
+    };
+
+    // if(canvasKeyCode == 32 || canvasKeyCode == 38) {
+    //   // if(!this.jumping){
+    //   //  this.jumping = true;
+    //   //  this.y += -this.dy;
+    //   // }
+    //   this.y += -this.dy;
+    //
+    //
+    //
+    //
+    // };
+    //
+    // if (this.y < 350) {
+    //   this.dy = -this.dy;
+    // }
+    //
+    // if (this.y > 475) {
+    //   this.y = 475;
+    // }
+    //
+    // this.dx *= frictionValue;
+    // //this.dy += gravityValue;
+    //
     // this.x += this.dx;
-    // this.y += this.dy;
+    // //this.y += this.dy;
+    //
+    // // if(this.y > 475){
+    // //     this.y = 475;
+    // //     this.dy = 0;
+    // //     this.jumping = false;
+    // // }
+    //
+    // // if (this.x >= arena.width - (this.x + this.radius)) {
+    // //     this.x = arena.width - (this.x + this.radius);
+    // // } else if (this.x + this.radius <= 0) {
+    // //     this.x = 0 + this.radius;
+    // // };
+    // //
+
 
     this.draw();
   };
@@ -70,6 +150,86 @@ showElements();
 
 
 
+// var canvas = document.getElementById("arena"),
+//     ctx = canvas.getContext("2d"),
+//     width = 500,
+//     height = 200,
+//     player = {
+//       x : width/2,
+//       y : height - 5,
+//       width : 5,
+//       height : 5,
+//       speed: 3,
+//       velX: 0,
+//       velY: 0,
+//       jumping: false
+//     },
+//     keys = [],
+//     friction = 0.8,
+//     gravity = 0.3;
+//
+// canvas.width = width;
+// canvas.height = height;
+//
+// function update(){
+//   //check keys
+//     if (keys[38] || keys[32]) {
+//         // up arrow or space
+//       if(!player.jumping){
+//        player.jumping = true;
+//        player.velY = -player.speed*2;
+//       }
+//     }
+//     // if (keys[39]) {
+//     //     // right arrow
+//     //     if (player.velX < player.speed) {
+//     //         player.velX++;
+//     //     }
+//     // }
+//     // if (keys[37]) {
+//     //     // left arrow
+//     //     if (player.velX > -player.speed) {
+//     //         player.velX--;
+//     //     }
+//     // }
+//
+//     player.velX *= friction;
+//
+//     player.velY += gravity;
+//
+//     player.x += player.velX;
+//     player.y += player.velY;
+//
+//     if (player.x >= width-player.width) {
+//         player.x = width-player.width;
+//     } else if (player.x <= 0) {
+//         player.x = 0;
+//     }
+//
+//     if(player.y >= height-player.height){
+//         player.y = height - player.height;
+//         player.jumping = false;
+//     }
+//
+//   ctx.clearRect(0,0,width,height);
+//   ctx.fillStyle = "red";
+//   ctx.fillRect(player.x, player.y, player.width, player.height);
+//
+//   requestAnimationFrame(update);
+// }
+//
+// document.body.addEventListener("keydown", function(e) {
+//     keys[e.keyCode] = true;
+// });
+//
+// document.body.addEventListener("keyup", function(e) {
+//     keys[e.keyCode] = false;
+// });
+//
+//
+// window.addEventListener("load",function(){
+//     update();
+// });
 
 
 
